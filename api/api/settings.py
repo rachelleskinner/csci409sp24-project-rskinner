5bf92d400a5876def50855a0b6a7657f6f469cdd
rachelleskinner <rskinner@coastal.edu>
2024-02-28 16:58:17 -0500

    Corrected Mistype

--- api/api/settings.py
+++ api/api/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.prostgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
